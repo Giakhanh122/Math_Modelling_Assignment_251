cc2f95577dc33ea4b0c0d8458e74704f90cc4645
tienbao-stack <baohonguyen06@gmail.com>
2025-12-10 13:24:31 +0700

    Add additional test cases to run.py

--- source/run.py
+++ source/run.py
@@ -2,4 +2,10 @@ from source import run
 
 test_case1 = ("report.pnml", [1,2,3])
 
+test_case2 = ("medium_deadlock.pnml", [1, 2, 3, 4, 5, 6, 7, 8])
+
+test_case3 = ("medium_no_deadlock.pnml", [5, 1, 1, 1, 1, 1, 1, 2])
+
 run(test_case1)
+run(test_case2)
+run(test_case3)
